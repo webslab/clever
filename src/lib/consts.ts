8fddc610117b26908b0cc5f52d3bab110904bb0e
kennycallado <kennycallado@hotmail.com>
2025-04-03 17:45:21 +0200

    UPDATE(package)
    - initial changes
      - base to clever
      - replace images
      - update styles
      - update index

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,16 +1,17 @@
-export const WEBSLAB_PROJECT = "base";
+export const WEBSLAB_PROJECT = "clever";
 export const WEBSLAB_DOMAIN = "ipsitec.es";
 export const WEBSLAB_TOKEN = "TOKEN"; // TODO: temporary, should be user token
 
 // export const WEBSLAB_SITE = `http://localhost:8000`;
 export const WEBSLAB_SITE = `https://${WEBSLAB_PROJECT}.${WEBSLAB_DOMAIN}`;
 
-export const SITE_TITLE = "Base";
-export const SITE_DESCRIPTION = "Welcome to Base!";
+export const SITE_TITLE = "Clever";
+export const SITE_DESCRIPTION = "Welcome to Clever!";
 
 // TODO: Change to production URL
 export const DB = {
 	url: `${WEBSLAB_SITE}/db`,
+	// url: `https://base.ipsitec.es/db`,
 	config: {
 		access: "user",
 		database: WEBSLAB_PROJECT,
